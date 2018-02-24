084c925d42c7cc10c7ef650f599b4771b86f76bf
Christopher Fox <cfox1290@gmail.com>
2018-02-23 20:18:00 -0700

    first-draft

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Maze from './2d-Maze/Maze2D';
+
+class App extends Component {
+  render() {
+    {Maze()}
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
